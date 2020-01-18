511ec131bc5babd8a6e2398b6d5e3ebc4a8c302e
Bartek <bartekd97@gmail.com>
2020-01-18 16:44:46 +0100

    Added icons, credits, hidden sound button, disabled leaderboards, improved transition animations

---
+++ failors/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject musicCross;
+    public GameObject vibrationCross;
+    public GameObject soundCross;
+
+    public Music musicScript;
+
+    private void Start()
+    {
+        UpdateCrosses();
+    }
+
+    public void ChangeMusicSetting()
+    {
+        musicScript.ChangeMusicSettings();
+        UpdateCrosses();
+    }
+    public void ChangeVibrationsSettings()
+    {
+        GameManager.instance.ChangeVibrationSettings();
+        UpdateCrosses();
+    }
+    public void ChangeSoundsSettings()
+    {
+        musicScript.ChangeSoundsSettings();
+        UpdateCrosses();
+    }
+
+    void UpdateCrosses()
+    {
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            if (PlayerPrefs.GetInt("Music") == 1)
+                musicCross.SetActive(false);
+            else
+                musicCross.SetActive(true);
+        }
+        else
+            musicCross.SetActive(false);
+
+
+
+        if (PlayerPrefs.HasKey("Vibrations"))
+        {
+            if (PlayerPrefs.GetInt("Vibrations") == 1)
+                vibrationCross.SetActive(false);
+            else
+                vibrationCross.SetActive(true);
+        }
+        else
+            vibrationCross.SetActive(false);
+
+
+
+        if (PlayerPrefs.HasKey("Sounds"))
+        {
+            if (PlayerPrefs.GetInt("Sounds") == 1)
+                soundCross.SetActive(false);
+            else
+                soundCross.SetActive(true);
+        }
+        else
+            soundCross.SetActive(false);
+    }
+}
