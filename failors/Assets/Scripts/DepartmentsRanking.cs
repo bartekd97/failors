40af070016e35e082c1f23379fa3a4b5ce3cca29
Bartek <bartekd97@gmail.com>
2020-02-03 13:23:14 +0100

    departments ranking config

---
+++ failors/Assets/Scripts/DepartmentsRanking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DepartmentsRanking : MonoBehaviour
+{
+    [Serializable]
+    public class DepartmentRow
+    {
+        public string name;
+        public RectTransform row;
+        public Text scoreCounter;
+    }
+
+    public float startPosY = -235.0f;
+    public float fullRowHeight = 138.0f;
+    public float minLength = 240.0f;
+    public float maxLength = 1040.0f;
+
+    public DepartmentRow[] departments;
+}
